9ad8de8e65ab1e0e34a439269b48c484af76dcd1
dome288 <58824824+dome288@users.noreply.github.com>
2021-11-04 20:05:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,10 @@
-
-function compute()
-
-{
-    if(principal.value < 1){
-        alert("Amount has to be a positive")
-        principal.focus()
-        return
-        
-    }
-    let interest = principal.value * years.value * rate.value /100;
-    yearInFuture = new Date() .getFullYear() + parseInt(years.value);
-    result = Document.getElementById("result")
-    result.innerHTML = "<p> Your deposit <mark>"+ principal.value +"</mark>,<br>an interest"
-    
-    
-}
-
-function refreshSlider(){
-    rate.value
-    document.getElementById("rateLabel").innerText = rate.value + "%"}
+function{
+    let p=document.getElementById('amount').value;
+    let t=document.getElementById('time').value;
+    let r=document.getElementById('rate').value;
 
 
-finishPage = () =>{
-    
-    var rate =document.getElementById("rate");
-    var principal = document.getElementById("principal");
-    var years = document.getElementById("years");
-    years = document.getElementById("years");
-    for(i=1; i<=10; i++){
-        let element = document.createElement("option")
-        element.innerText = i
-        element.setAttribute("value", i)
-        years.appendChild(element)
-    
-    
-    }
-    
-}
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    let SI=(p*t*r)/100;
 
-    p=document.getElementById("principal")
-     
-
+    document.getElementById('si').innerHTML="Total Simple Interest Is: " +SI;
 }
